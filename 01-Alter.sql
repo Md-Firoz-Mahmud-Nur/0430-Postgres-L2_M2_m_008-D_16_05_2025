86fa44427d2a322ebcec10f60bdcea36af21655e
firoz.nur <firoznur5@gmail.com>
2025-04-21 11:33:43 +0600

    Add : Alter Table Modification

---
+++ 01-Alter.sql
@@ -0,0 +1,21 @@
+-- Active: 1747889982201@@127.0.0.1@5432@ph
+
+SELECT * from person4;
+
+ALTER TABLE person2
+    ADD COLUMN email VARCHAR(25)  DEFAULT 'default@mail.com' NOT NULL;
+
+ALTER TABLE person2
+    DROP COLUMN email;
+
+ALTER TABLE person2
+    RENAME COLUMN age to user_age;
+
+ALTER TABLE person2
+    ALTER COLUMN user_name TYPE VARCHAR(50);
+
+ALTER TABLE person2
+    ALTER COLUMN user_age set UNIQUE;
+
+ALTER TABLE person2
+    ALTER COLUMN user_age drop NOT null;
