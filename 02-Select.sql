09dd885b2020fb99e20e4ad203667386e7b436a2
firoz.nur <firoznur5@gmail.com>
2025-04-24 15:50:47 +0600

    Try : Where Select

--- 02-Select.sql
+++ 02-Select.sql
@@ -35,3 +35,26 @@ VALUES
     ('Miam', 'Wright', 19, 'C', 'Math', 'liam.wright@example.com', '2002-06-14', 'A+', 'New Zealand');
 
 SELECT * FROM students ORDER BY age DESC;
+
+SELECT DISTINCT blood_group from students;
+
+-- data filtering
+
+-- Select students from the USA
+-- Select students with 'A' grade in Physics
+-- Select students with a specific blood group ('A+')
+-- Select students from the USA or from Australia
+-- Select students from the USA or from Australia and the age is 20
+-- Select students with a grade of 'A' or 'B' in Math or Physics:
+
+-- Select students older than 20
+
+SELECT * from students
+    WHERE (country = 'USA' OR country = 'Australia') and age = 20;
+
+SELECT * from students
+    WHERE country <> 'USA' ;
+
+SELECT * from students;
+
+SELECT length(first_name) from students;
